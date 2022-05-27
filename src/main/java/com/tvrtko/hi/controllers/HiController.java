2cac0b344fa1fcaba6bccb05f6439170be54d32a
Maksim <maksim.borkovic@gmail.com>
2022-05-27 14:51:40 +0200

    Added solution using RequestParam

--- src/main/java/com/tvrtko/hi/controllers/HiController.java
+++ src/main/java/com/tvrtko/hi/controllers/HiController.java
@@ -2,16 +2,21 @@ package com.tvrtko.hi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HiController {
     
     @GetMapping(value = "/hey")
-    public String SayHi(String name) {
+    public String SayHi(@RequestParam String name) {
+        return "Hey " + name + "!";
+    }
+
+    @GetMapping(value = "/hey1")
+    public String SayHi1(String name) {
         if (name == null)
         return "Hey man/woman";
         else
         return "Hey " + name + "!";
     }
-
 }
